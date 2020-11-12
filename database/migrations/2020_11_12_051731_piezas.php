eda0a6844076ba656d15a9d2e192dc952d7acd8c
LeoPD15 <leon_xlr8@hotmail.com>
2020-11-11 23:22:36 -0600

    migracion

---
+++ database/migrations/2020_11_12_051731_piezas.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Piezas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Piezas', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre');
+            $table->string('Descripcion');
+            $table->integer('Num_piezas');
+            $table->float('Costo_pieza');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
